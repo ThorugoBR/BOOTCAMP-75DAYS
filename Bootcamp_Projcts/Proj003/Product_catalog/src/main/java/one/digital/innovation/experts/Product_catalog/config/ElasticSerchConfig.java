351276b730a43f15bb7e354f0e6e14c2165e3d3f
ThorugoBR <thorugo@outlook.com>
2021-08-03 10:32:16 -0300

    new_Proj

---
+++ Bootcamp_Projcts/Proj003/Product_catalog/src/main/java/one/digital/innovation/experts/Product_catalog/config/ElasticSerchConfig.java
@@ -0,0 +1,8 @@
+package one.digital.innovation.experts.Product_catalog.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSerchConfig {
+}
---
+++ Bootcamp_Projcts/Proj003/Product_catalog/src/test/java/one/digital/innovation/experts/Product_catalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digital.innovation.experts.Product_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
