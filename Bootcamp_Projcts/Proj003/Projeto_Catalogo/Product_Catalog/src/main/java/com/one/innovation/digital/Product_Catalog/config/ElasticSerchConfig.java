772e576cf6da33d3bed83c86fb6e39e0fcb9a69a
ThorugoBR <thorugo@outlook.com>
2021-08-05 13:49:14 -0300

    greenlight

---
+++ Bootcamp_Projcts/Proj003/Projeto_Catalogo/Product_Catalog/src/main/java/com/one/innovation/digital/Product_Catalog/config/ElasticSerchConfig.java
@@ -0,0 +1,45 @@
+package com.one.innovation.digital.Product_Catalog.config;
+
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchEntityMapper;
+import org.springframework.data.elasticsearch.core.EntityMapper;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "one.digital.innovation.experts.Product_catalog.repository")
+public class ElasticSerchConfig extends AbstractElasticsearchConfiguration {
+
+    public RestHighLevelClient ElasticSearchClient(){
+        ClientConfiguration clientConfiguration=ClientConfiguration.builder()
+                .connectedTo("localhost:8080","localhost:8080")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+
+    }
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        return null;
+    }
+
+    @Bean
+    @Override
+    public EntityMapper entityMapper(){
+        ElasticsearchEntityMapper entityMapper=new ElasticsearchEntityMapper(elasticsearchMappingContext(), new DefaultConversionService());
+        entityMapper.setConversions(elasticsearchCustomConversions());
+        return entityMapper;
+
+    }
+
+
+}
+
+
---
+++ Bootcamp_Projcts/Proj003/Projeto_Catalogo/Product_Catalog/src/main/java/com/one/innovation/digital/Product_Catalog/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.one.innovation.digital.Product_Catalog.repository;
+
+
+import com.one.innovation.digital.Product_Catalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ProductRepository extends CrudRepository<Product,Integer> {
+
+
+}
