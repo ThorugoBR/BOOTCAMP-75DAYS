6737810238a73cf2081e30c7bd8a575bf4f37480
ThorugoBR <thorugo@outlook.com>
2021-08-05 09:33:18 -0300

    ok

--- Bootcamp_Projcts/Proj003/Product_catalog/src/main/java/one/digital/innovation/experts/Product_catalog/repository/ProductRepository.java
+++ Bootcamp_Projcts/Proj003/Product_catalog/src/main/java/one/digital/innovation/experts/Product_catalog/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package one.digital.innovation.experts.Product_catalog.repository;
 
 import one.digital.innovation.experts.Product_catalog.model.Product;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.CrudRepositoryExtensionsKt;
+
 
 public interface ProductRepository extends CrudRepository<Product,Integer> {
 
