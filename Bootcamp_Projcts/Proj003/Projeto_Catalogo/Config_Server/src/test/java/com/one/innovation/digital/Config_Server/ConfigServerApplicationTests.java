e6657f634f5098adda3fc320712f3e27a23efbab
ThorugoBR <thorugo@outlook.com>
2021-08-05 14:05:42 -0300

    up

---
+++ Bootcamp_Projcts/Proj003/Projeto_Catalogo/Config_Server/src/test/java/com/one/innovation/digital/Config_Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.innovation.digital.Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Bootcamp_Projcts/Proj003/Projeto_Catalogo/Service_Discovery/src/test/java/com/one/innovation/digital/Service_Discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.innovation.digital.Service_Discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
