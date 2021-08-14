78943afae1473d5167512a3177ed21eb8243a1c3
ThorugoBR <thorugo@outlook.com>
2021-08-14 14:00:36 -0300

    lu

--- Angular/course-manager/src/app/app.module.ts
+++ Angular/course-manager/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-//import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 //import { countReset } from 'console';
 import { CourseListComponent } from './courses/course-list-component';
@@ -20,8 +20,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    //AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',redirectTo: 'courses',pathMatch:'full'
+      },
+      {
+        path:'courses',component:CourseListComponent
+      }
+    ]) ,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
