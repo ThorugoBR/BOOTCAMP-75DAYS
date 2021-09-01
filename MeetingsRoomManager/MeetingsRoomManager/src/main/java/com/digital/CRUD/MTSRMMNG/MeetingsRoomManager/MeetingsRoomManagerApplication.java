3940a2ad6e233ed674c7df0cabb1e113ed4e322e
ThorugoBR <thorugo@outlook.com>
2021-09-01 19:50:20 -0300

    update

---
+++ MeetingsRoomManager/MeetingsRoomManager/src/main/java/com/digital/CRUD/MTSRMMNG/MeetingsRoomManager/MeetingsRoomManagerApplication.java
@@ -0,0 +1,13 @@
+package com.digital.CRUD.MTSRMMNG.MeetingsRoomManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsRoomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsRoomManagerApplication.class, args);
+	}
+
+}
---
+++ MeetingsRoomManager/MeetingsRoomManager/src/test/java/com/digital/CRUD/MTSRMMNG/MeetingsRoomManager/MeetingsRoomManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.CRUD.MTSRMMNG.MeetingsRoomManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsRoomManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
