cf8517c401ba4a78c377a915e2dac64ccc3c5bd8
ThorugoBR <thorugo@outlook.com>
2021-09-04 18:32:53 -0300

    up

--- MeetingsRoomManager/Front/front/src/app/room-list/room-list.component.ts
+++ MeetingsRoomManager/Front/front/src/app/room-list/room-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../room';
+import { RoomDetailsComponent } from '../room-details/room-details.component';
+import { Observable } from 'rxjs';
+import { RoomService } from '../room.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+ rooms!: Observable<Room[]>;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private roomService:RoomService, private router: Router) {}
+
+  ngOnInit(){
+    this.reloadData();
   }
 
+  reloadData(){
+    this.rooms=this.roomService.getRoomsList();
+  }
+
+  deleteRoom(id:number){
+    this.roomService.deleteRoom(id)
+    .subscribe(
+      data => {
+        console.log(data);
+        this.reloadData();
+      },
+      error => console.log(error));
+  }
+  
+ roomDetails(id:number){
+   this.router.navigate(['details',id]);
+
+ }
+
+ updateRoom(id:number){
+  this.router.navigate(['update',id]);
+
 }
+
+}
+
+
